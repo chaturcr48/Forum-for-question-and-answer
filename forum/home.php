0f292708d6d7c273655294175e7622bf3ff75ff5
Kumar Chatur Ranjan <kumarchaturranjan48@gmail.com>
2020-09-14 10:37:17 +0530

    Update home.php

--- forum/home.php
+++ forum/home.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+    <?php include "partials/dbconnect.php"; ?>
     <?php include "partials/_header.php"; ?>
     
     <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
