d31da27ee9e83190c3c934649610d581045496a7
Kumar Chatur Ranjan <kumarchaturranjan48@gmail.com>
2020-09-19 11:16:34 +0530

    Update about.php

--- forum/about.php
+++ forum/about.php
@@ -14,6 +14,7 @@
 </head>
 
 <body>
+    <?php include "partials/dbconnect.php"; ?>
     <?php include "partials/_header.php"; ?>
     <?php include "partials/footer.php"; ?>
     <!-- Optional JavaScript -->
