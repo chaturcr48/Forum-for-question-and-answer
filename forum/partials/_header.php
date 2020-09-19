caf7ccf932846b72c8795e9399e50a58b6e674b2
Kumar Chatur Ranjan <kumarchaturranjan48@gmail.com>
2020-09-19 11:04:02 +0530

    Update _header.php

--- forum/partials/_header.php
+++ forum/partials/_header.php
@@ -39,8 +39,8 @@ echo'
                 <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contacts</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <form class="form-inline my-2 my-lg-0" action="/CHATUR_PHP/forum/search.php" method="get">
+            <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 mr-1 my-sm-0" type="submit">Search</button>
         </form>';
         if(!$loggedin){
